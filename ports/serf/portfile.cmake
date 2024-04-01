a13725d5719fecab7d56a90d1340b94f5b0a1d33
rinrab <tima@chemodax.net>
2024-04-01 18:14:28 +0200

    [serf] change build command
    * Use vcpkg_install_msbuild() instead of building project via vcpkg_build_msbuild() and later manual installation.
    
    This method is better because of:
    * Deprecation of the vcpkg_build_msbuild() command.
    * The vcpkg_install_msbuild() does not require to copy files manually.

--- ports/serf/portfile.cmake
+++ ports/serf/portfile.cmake
@@ -11,8 +11,9 @@ vcpkg_extract_source_archive(
         "serf-msbuild.patch"
 )
 
-vcpkg_build_msbuild(
-    PROJECT_PATH "${SOURCE_PATH}/serf.sln"
+vcpkg_install_msbuild(
+    SOURCE_PATH "${SOURCE_PATH}"
+    PROJECT_SUBPATH "serf.sln"
     TARGET "serf"
     OPTIONS
         /p:CURRENT_INSTALLED_DIR=${CURRENT_INSTALLED_DIR}
@@ -21,17 +22,6 @@ vcpkg_build_msbuild(
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 
-SET(RELEASE_OUT_DIR "${SOURCE_PATH}/out/Release-${VCPKG_TARGET_ARCHITECTURE}")
-SET(DEBUG_OUT_DIR "${SOURCE_PATH}/out/Debug-${VCPKG_TARGET_ARCHITECTURE}")
-
 # Header files
 file(GLOB_RECURSE INCLUDES "${SOURCE_PATH}/*.h")
 file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-
-# Lib files
-file(GLOB_RECURSE LIBS_RELEASE "${RELEASE_OUT_DIR}/*.lib")
-file(GLOB_RECURSE LIBS_DEBUG "${DEBUG_OUT_DIR}/*.lib")
-file(COPY ${LIBS_RELEASE} DESTINATION "${CURRENT_PACKAGES_DIR}/lib")
-file(COPY ${LIBS_DEBUG} DESTINATION "${CURRENT_PACKAGES_DIR}/debug/lib")
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
