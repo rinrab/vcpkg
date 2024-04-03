8fa70d46792f37da2a373c9807ff75779f08d720
rinrab <tima@chemodax.net>
2024-04-03 17:13:45 +0200

    [serf] fix CMake targets * Add serf-config.cmake file. * Add generating of cmake targets from serf-config.cmake. * Install serf-config.cmake from portfile.cmake. * No changes in usage are needed.

--- ports/serf/portfile.cmake
+++ ports/serf/portfile.cmake
@@ -29,3 +29,5 @@ file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
 configure_file("${SOURCE_PATH}/build/serf.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/serf.pc" @ONLY)
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
+
+file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/serf-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
