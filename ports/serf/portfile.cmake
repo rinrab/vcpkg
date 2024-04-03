92782ec97af3d7cefdf03fc688d8bd99492bb9f2
rinrab <tima@chemodax.net>
2024-04-03 15:58:29 +0200

    [serf] use package config file from upstream instead of custom.

--- ports/serf/portfile.cmake
+++ ports/serf/portfile.cmake
@@ -27,5 +27,5 @@ vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
 file(GLOB_RECURSE INCLUDES "${SOURCE_PATH}/*.h")
 file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
 
-configure_file("${CURRENT_PORT_DIR}/serf.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/serf.pc" @ONLY)
+configure_file("${SOURCE_PATH}/build/serf.pc.in" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/serf.pc" @ONLY)
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
